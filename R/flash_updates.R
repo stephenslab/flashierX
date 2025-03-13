5f296487b7c3c8252a789b0e9e52c9c31f651e93
junmingguan <260361260@qq.com>
2025-03-12 21:58:07 -0700

    Add sequential F update, tau update and 'fake' flashier backfit

--- R/flash_updates.R
+++ R/flash_updates.R
@@ -45,20 +45,46 @@ update_flashier_F <- function(flash_obj) {
 }
 
 
+#' Update loading F sequentially in a flash object in which factor L is fixed.
+#'
+#' @param flash_obj flash object
+#'
+#' @return flash object with updated loading L
+#' @export
+update_flashier_F_sequentially <- function(flash_obj) {   #, update_tau=FALSE) {
+  # flash_obj <- flash_factors_unfix(flash_obj, kset = 1:flash_obj$n_factors)
+  # flash_obj <- flash_factors_fix(
+  #   flash_obj, kset = 1:flash_obj$n_factors,
+  #   which_dim = "loadings",
+  #   fixed_idx = NULL,
+  #   use_fixed_in_ebnm = TRUE
+  # )
+  flash_obj <- suppressWarnings(
+    flash_backfit(
+      flash_obj, maxiter = 1, verbose = 0, extrapolate = FALSE, update_tau = F, F_or_L = 'factors'
+    )
+  )
+  return(flash_obj)
+}
+
+
 #' Update loading L in a flash object in which factor F is fixed.
 #'
 #' @param flash_obj flash object
 #'
 #' @return flash object with updated loading L
 #' @export
-update_flashier_L <- function(flash_obj) {
-  if ((length(unlist(flash_obj$flash_fit$fix.dim)) == 0) || 
-        (any(unlist(flash_obj$flash_fit$fix.dim) != 2))) {
-    stop("Factors are not fixed.")
-  }
+update_flashier_L <- function(flash_obj) {  # , init_tau_for_L=TRUE) {
+  # flash_obj <- flash_factors_unfix(flash_obj, kset = 1:flash_obj$n_factors)
+  # flash_obj <- flash_factors_fix(
+  #   flash_obj, kset = 1:flash_obj$n_factors,
+  #   which_dim = "factors",
+  #   fixed_idx = NULL,
+  #   use_fixed_in_ebnm = TRUE
+  # )
   flash_obj <- suppressWarnings(
     flash_backfit(
-      flash_obj, maxiter = 1, verbose = 0
+      flash_obj, maxiter = 1, extrapolate = FALSE, verbose = 0, F_or_L = 'loadings', update_tau = TRUE #, init_tau_for_L = init_tau_for_L
     )
   )
   return(flash_obj)
@@ -70,23 +96,29 @@ update_flashier_L <- function(flash_obj) {
 #'
 #' @return flash object
 #' @export
-flash_backfitX <- function(flash_obj, maxiter = 2000, tol = NULL) {
+flash_backfitX <- function(flash_obj, update_F_sequentially = FALSE, update_tau = FALSE, maxiter = 2000, tol = NULL, update_order = c(1, 2)) {
   if (is.null(tol)) {
     tol <- flash_obj$flash_fit$conv.tol
   }
-  # fix factors so that it won't be updated during backfit
-  flash_obj <- flash_factors_fix(
-    flash_obj, kset = 1:flash_obj$n_factors,
-    which_dim = "factors",
-    fixed_idx = NULL,
-    use_fixed_in_ebnm = TRUE
-  )
   old.elbo <- flash_obj$elbo
   iter <- 0
   elbo.df <- data.frame()
   while (TRUE) {
-    flash_obj <- update_flashier_F(flash_obj)
-    flash_obj <- update_flashier_L(flash_obj)
+    # print(flash_obj$flash_fit$tau)
+    for (ord in update_order) {
+      if (ord == 1) {
+        flash_obj <- update_flashier_L(flash_obj)
+      } else if (ord == 2) {
+        if (update_F_sequentially) {
+          flash_obj <- update_flashier_F_sequentially(flash_obj)
+        } else {
+          flash_obj <- update_flashier_F(flash_obj)
+        }
+        if (update_tau) {
+          flash_obj <- update_flashier_tau(flash_obj)
+        }
+      }
+    }
     new.elbo <- flash_obj$elbo
     iter <- iter + 1
     elbo.df <- rbind(elbo.df, data.frame(iter = iter, elbo = new.elbo))
@@ -101,3 +133,62 @@ flash_backfitX <- function(flash_obj, maxiter = 2000, tol = NULL) {
   flash_obj$elbo.df <- elbo.df
   return(flash_obj)
 }
+
+#' Update flash object using the classic iterative scheme.
+#'
+#' @param flash_obj flash object
+#'
+#' @return flash object
+#' @export
+flash_backfitX_imitate_classic <- function(flash_obj, maxiter = 2000, tol = NULL, update_order = c(1, 2)) {
+  if (is.null(tol)) {
+    tol <- flash_obj$flash_fit$conv.tol
+  }
+  old.elbo <- flash_obj$elbo
+  iter <- 0
+  elbo.df <- data.frame()
+  K <- flash_obj$n_factors
+  while (TRUE) {
+    for (k in 1:K) {
+      # for (ord in update_order) {
+        # if (ord == 1) {
+          flash_obj <- suppressWarnings(
+            flash_backfit(
+              flash_obj, maxiter = 1, verbose = 0, extrapolate = FALSE, update_tau = FALSE, F_or_L = 'loadings', kset = k #, init_tau_for_L = F
+            )
+          )
+        # } else if (ord == 2) {
+          flash_obj <- suppressWarnings(
+            flash_backfit(
+              flash_obj, maxiter = 1, verbose = 0, extrapolate = FALSE, update_tau = TRUE, F_or_L = 'factors', kset = k
+            )
+          )
+        # }
+      # }
+      
+    }
+    new.elbo <- flash_obj$elbo
+    iter <- iter + 1
+    elbo.df <- rbind(elbo.df, data.frame(iter = iter, elbo = new.elbo))
+    if ((iter == maxiter) || (abs(new.elbo - old.elbo) <= tol)) {
+      break
+    }
+    old.elbo <- new.elbo
+    # cat('elbo: ', old.elbo, '\n')
+  }
+  cat('Number of iterations: ', iter, '\n')
+  cat('Final elbo: ', flash_obj$elbo, '\n')
+  flash_obj$elbo.df <- elbo.df
+  return(flash_obj)
+}
+
+
+
+### Helpers ---------------
+update_flashier_tau <- function(flash_obj) {
+  flash_fit_obj <- get.fit(flash_obj)
+  flash_fit_obj <- init.tau(flash_fit_obj)
+  flash_fit_obj <- set.obj(flash_fit_obj, calc.obj(flash_fit_obj)) 
+  return(wrapup.flash(flash_fit_obj, 3L))
+}
+
